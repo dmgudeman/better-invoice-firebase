3aeaa4c8a203a17be4d2a3fbac4798a7ae8c1f6b
davidmgudeman <davidmgudeman@gmail.com>
2017-07-16 15:35:35 -0700

    company-edit: fixed the ngAfterViewInit method

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -97,8 +97,8 @@ export class CompanyEditComponent implements OnInit, AfterViewInit {
       this.title = this.coId ? " Edit "+ this.company.name + " Details" : " New Business";
   }
   ngAfterViewInit(){
-    if(this.company){
-    this.addressViewChild.myform.setValue(this.company.address);
+    if(this.company && this.company.address){
+      this.addressViewChild.myform.setValue(this.company.address);
     }
 
   }
