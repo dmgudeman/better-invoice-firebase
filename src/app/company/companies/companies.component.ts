ce6bbdca23d5b63c7da2dad4e7a908f54bf83b87
davidmgudeman <davidmgudeman@gmail.com>
2017-07-22 16:31:52 -0700

    companies: fixed the route to the data so identified user's companies show.

--- src/app/company/companies/companies.component.ts
+++ src/app/company/companies/companies.component.ts
@@ -66,86 +66,23 @@ export class CompaniesComponent implements OnInit {
   };
 
   ngOnInit() {
-  // console.log(this.companiesArray);
-  //    if(!this.user){ 
-  //     console.log('NOT LOGGED IN')
-  //     return;
-  //   }
-  //   console.log("LOGGED IN", this.user)
     this.afAuth.authState.subscribe ( user => {
       if (!user) return;
       this.userId = user.uid;
       
-      firebase.database().ref('/companies').once('value', (snapshot)=> {
+      firebase.database().ref('/companiesByUserId/' + this.userId).once('value', (snapshot)=> {
         this.companiesArray =  (<any>Object).values(snapshot.val());
       })
-    //   let companiesRef = firebase.database().ref('/companiesByUserId/' + this.userId);
-    //   companiesRef.on('value', gotData, errData);
-
-    //  function gotData(data, setData){
-    //     // console.log((<any>Object).values(data.val()));
-    //     let companies = data.val();
-    //     var keys = Object.keys(data.val());
-    //     let array =[];
-    //     for(let i=0; i< keys.length; i++) {
-    //       var k = keys[i];
-    //       (<any>array).push(companies[k])
-    //       // console.log( companies[k]);
-
-    //     }
-    //     this.companiesArray = array;
-    //   };
-     
-    //   function errData(err){
-    //     console.log('Error');
-    //     console.log(err);
-    //   }
-
-    //   function setData(data){
-    //     console.log('data', data);
-    //     this.companiesArray = data;
-    //     console.log('this.companiesArray', this.companiesArray);
-
-
-      })
-    //   companiesRef.on('value', function(snapshot){
-    //     console.log('snapshot', snapshot.val());
-    //     console.log('BBBBBBBBBBBBB', snapshot.val() === null)
-    //     this.companiesArray = (<any>Object).values(snapshot.val())
-    //   });
-      
-    //   this.db.list('companies').subscribe(x=>{
-    //     this.x = x;
-    // }
-
-
-    // )
-    // firebase.database().ref().child('companies').once('value').then(function(snapshot){
-
-    //   console.log('snapshot', snapshot);
-    //   return snapshot;
-    //   // this.companiesArray = snapshot.val();
-    // }).then(data =>{
-    //   this.companiesArray = (<any>Object).values(data);
-    //   console.log(this.companiesArray);
-
-    // });
-      
-    // });
-   
+    })
   }
 
-  getCompanies(a){
-    console.log('AAAAAAAAAAAAAA', a);
-
-  }
   goToEditCompany() {
     this.router.navigate(['company-edit' ]);
   
   }
-onLogOut() {
+  onLogOut() {
 
-}
+  }
 
 }
 
