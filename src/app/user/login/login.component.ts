af2e6ce2cecf7ce812e690068f19bd5404a4365e
davidmgudeman <davidmgudeman@gmail.com>
2017-08-02 15:32:56 -0700

    login: it works both ways to create an account. Facebook first then link google to facebook credentials or vice versa

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -49,65 +49,30 @@ export class LoginComponent implements OnInit {
   }
   
   loginWithGoogle() {
+    let auth = this.afAuth.auth;
     // Step 1.
-// User tries to sign in to Google.
-  firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function(error) {
-   console.log('error.code', error['code']);
-  // An error happened.
-  if (error['code'] === 'auth/account-exists-with-different-credential') {
-    // Step 2.
-    // User's email already exists.
-    // The pending Google credential.
-    var pendingCred = error['credential'];
-    // The provider account's email address.
-    var email = error['email'];
-    // Get registered providers for this email.
-    firebase.auth().fetchProvidersForEmail(email).then(function(providers) {
-      console.log('providers', providers);
-      // Step 3.
-      // If the user has several providers,
-      // the first provider in the list will be the "recommended" provider to use.
-      // if (providers[0] === 'password') {
-      //   // Asks the user his password.
-      //   // In real scenario, you should handle this asynchronously.
-      //   // var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
-      //   auth.signInWithEmailAndPassword(email, password).then(function(user) {
-      //     // Step 4a.
-      //     return user.link(pendingCred);
-      //   }).then(function() {
-      //     // Google account successfully linked to the existing Firebase user.
-      //     goToApp();
-      //   });
-      //   return;
-      // }
-      // All the other cases are external providers.
-      // Construct provider object for that provider.
-      // TODO: implement getProviderForProviderId.
-      // var provider = getProviderForProviderId(providers[0]);
-      var provider = new firebase.auth.FacebookAuthProvider();
-      // At this point, you should let the user know that he already has an account
-      // but with a different provider, and let him validate the fact he wants to
-      // sign in with this provider.
-      // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
-      // so in real scenario you should ask the user to click on a "continue" button
-      // that will trigger the signInWithPopup.
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // Remember that the user may have signed in with an account that has a different email
-        // address than the first one. This can happen as Firebase doesn't control the provider's
-        // sign in flow and the user is free to login using whichever account he owns.
-        // Step 4b.
-        // Link to Google credential.
-        // As we have access to the pending credential, we can directly call the link method.
-        result.user.link(pendingCred).then(function() {
-          // Google account successfully linked to the existing Firebase user.
-          // goToApp();
-        });
-      });
-    });
-  }
-});
-
-    // })
+    // User tries to sign in to Google.
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function(error) {
+      console.log('error.code', error['code']);
+      // An error happened.
+      if (error['code'] === 'auth/account-exists-with-different-credential') {
+        // Step 2.
+        // User's email already exists.
+        // The pending Google credential.
+        var pendingCred = error['credential'];
+        // The provider account's email address.
+        var email = error['email'];
+        // Get registered providers for this email.
+        firebase.auth().fetchProvidersForEmail(email).then(function(providers) {
+          console.log('providers', providers);
+          if (providers[0] === 'facebook.com'){
+            auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).catch(error =>{
+              console.log('Error in loginWithFacebook', error);
+            });
+          }
+        })
+      }
+    })
   }
 loginWithFacebook() {
   let auth = this.afAuth.auth;
@@ -129,10 +94,8 @@ loginWithFacebook() {
       if (providers[0] === 'google.com'){
         auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(error =>{
           console.log('Error in loginWithFacebook', error);
-        })
-
+        });
       }
-    
     })
     }
   })
